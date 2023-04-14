ccdc6b98ba3ea5d3a9aead5e2951336c86e6a754
Thom Sterenborg <thomsterenborg@gmail.com>
2023-04-14 19:24:58 +0200

    Less zoom on hover

--- frontend/src/components/ui/FeaturedEvent.jsx
+++ frontend/src/components/ui/FeaturedEvent.jsx
@@ -8,7 +8,7 @@ import { EventTitle } from "./EventTitle";
 
 export const FeaturedEvent = ({ title, image, location, startTime }) => {
   return (
-    <div className="zoomin animation-duration-100 p-card flex flex-column md:flex-row max-w-1200 w-full md:h-20rem  select-none highlighted-event">
+    <div className="zoomin animation-duration-100 p-card flex flex-column md:flex-row max-w-1200 w-full md:h-20rem  select-none highlighted-event shadow-6">
       <div className="w-full md:w-6 m-4">
         <h1 className="text-6xl text-gray-200 uppercase">featured.</h1>
         <EventTitle title={title} />
